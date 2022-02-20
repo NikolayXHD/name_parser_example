e2788554cd154e62fd38954ecd63e769ff524a18
NikolayXHD <hidalgo1984@list.ru>
2022-02-20 05:09:39 +0300

    problem 1.1

--- notebooks/test_ml_nlp/notebook.py
+++ notebooks/test_ml_nlp/notebook.py
@@ -37,12 +37,9 @@
 #
 # Для удобства - первые `max(N, 10)` предложений после заголовка.
 
-# %% id="nxItTfgYH8tf" executionInfo={"status": "ok", "timestamp": 1645301949622, "user_tz": -180, "elapsed": 646, "user": {"displayName": "Nikolay Hidalgo Diaz", "photoUrl": "https://lh3.googleusercontent.com/a-/AOh14Gi1_siNR5s5IeeMXEevVauMdR6PDCN9Z3UsWxRFQw=s64", "userId": "13222971951237205321"}}
-import wikipedia
-import random
-
 # %% id="rZQv8GeKHp45" executionInfo={"status": "ok", "timestamp": 1645301955392, "user_tz": -180, "elapsed": 260, "user": {"displayName": "Nikolay Hidalgo Diaz", "photoUrl": "https://lh3.googleusercontent.com/a-/AOh14Gi1_siNR5s5IeeMXEevVauMdR6PDCN9Z3UsWxRFQw=s64", "userId": "13222971951237205321"}}
 import wikipedia
+import random
 
 def get_wiki_articles(articles, lang='ru', n=10):
     """Extract text from a Wikipedia page.
@@ -116,8 +113,236 @@ articles
 # _**Nota bene!** Использовать можно всё, что угодно. Общих решений достаточно, не нужно заморачиваться с частными случаями и излишней предобработкой. Результаты можно выводить любым удобным способом._
 #
 
-# %% id="aIxuw9S7SCa0"
-### Your code here
+# %% [markdown] id="aIxuw9S7SCa0"
+# ## Задача 1
+
+# %% [markdown]
+# Так как текст русскоязычный, используем razdel из пакета natasha
+
+# %%
+# !pip install natasha
+
+# %%
+import razdel
+
+def tokenize(txt):
+    assert isinstance(txt, str)
+    return razdel.tokenize(txt)
+
+
+EXAMPLE_TEXT = (
+    '- Петя, налей \tCOO3,(OH4)2,\n'
+    'просит Кузнецов И.И. - где-то в С.Ш.А. шёл 2022г.?! и т.д. и т.п.'
+)
+
+print(
+    list(
+        token.text for token in tokenize(EXAMPLE_TEXT)
+    )
+)
+
+# %% [markdown]
+# ```
+# [+] Числа отдельный токен
+#   2022г -> 2022 + г
+# [-] химические формулы отдельный токен
+#   COO3,(OH4)2 распались на более мелкие токены, скобки и т.д.
+# [+] Слова с дефисом - отдельный токен, включая дефис
+#   "где-то" попал в 1 токен
+# ```
+# Требование по химическим формулам сходу не удовлетворено.
+# Что будем делать? Метод `razdel.tokenize` не имеет каких-то настроек.
+# Документация и исходный код razdel говорят, что алгоритм основан на правилах.
+#
+# 2 варианта действий. 
+# * токенизировать руками
+#   + можно написать очень быстро
+#   - выигрыш качества за счёт логики razdel будет утерян
+# * разобраться в механизме правил razdel, модифицировать список правил так, чтобы хим. формулы были токенизированы как мы хотим
+#   + не жертвуем качеством токенизации
+#   - может оказаться сложнее чем кажется
+
+# %% [markdown]
+# ### Быстрый способ
+
+# %%
+import re
+import string
+
+
+def split_words_by_regex(txt):
+    return (
+        token for token in re.split(r'\s+', txt)
+        if not all(char in string.punctuation for char in token)
+    )
+
+
+def count_words_by_regex(txt):
+    return sum(1 for _ in split_words_by_regex(txt))
+
+tokens = list(split_words_by_regex(EXAMPLE_TEXT))
+print(f'{len(tokens)} слов: {tokens}')
+
+# %% [markdown]
+# ```
+# [-] Числа отдельный токен
+#   "2022г" попал в 1 токен, "г" не был отделён
+# [+] химические формулы отдельный токен
+#   COO3,(OH4)2 попал в 1 токен
+# [+] Слова с дефисом - отдельный токен, включая дефис
+#   "что-то" попал в 1 токен
+# ```
+# Числа можно отделить, например ещё 1 шагом re.split после разделения по пробелам.
+# Здесь не делаю в целях экономии времени, т.к. принцип понятен.
+
+# %%
+# для русскоязычных текстов string.punctuation вроде бы достаточен,
+# но нужно понимать, что даже не особо экзотичный язык, такой
+# как испанский, уже потребует более аккуратного подхода:
+
+print(string.punctuation)
+print(f'¿: {"¿" in string.punctuation}')
+
+# символ троеточия … может где-то проскользнуть
+print(f'…: {"…" in string.punctuation}')
+
+# %%
+for topic, article in zip(topics, articles):
+    print(f'{topic}: {count_words_by_regex(article)} слов')
+
+# %% [markdown]
+# ### Аккуратный способ
+
+# %%
+from razdel.segmenters.tokenize import (
+    Rule2112,
+    TokenSegmenter,
+    INT,
+    LAT,
+    PUNCT,
+    RULES,
+)
+from razdel.rule import Rule, JOIN
+from razdel.segmenters.base import DebugSegmenter
+
+class ChemicalRule(Rule):
+    """
+    Правило соеднинения атомов razdel в токены
+    для группировки химических формул в отдельный токен.
+    
+    <formula> ::= (Latin|Digit|Parenthesis)([,]Latin|Digit|Parenthesis)*
+    
+    Понятно, что это грубый и, вообще говоря, некорректный способ,
+    в частности мы не можем так проконтролировать корректность скобочной структуры
+    в формуле.
+    """
+    name = 'chemical'
+
+    def __call__(self, split):
+#         print(
+#             f'L3: {split.left_3!r} L2: {split.left_2} L1: {split.left_1}\n'
+#             f'D:{split.delimiter!r}\n'
+#             f'R1: {split.right_1!r} R2: {split.right_2!r} R3: {split.right_3!r}\n\n'
+#         )
+        if (
+            self._maybe_formula_boundary(split.left_1) 
+            and self._maybe_formula_boundary(split.right_1)
+        ):
+            return JOIN
+        
+        if (
+            self._maybe_formula_punctuation(split.left_1)
+            and self._maybe_formula_boundary(split.left_2)
+            and self._maybe_formula_boundary(split.right_1)
+        ):
+            return JOIN
+        
+        if (
+            self._maybe_formula_punctuation(split.right_1)
+            and self._maybe_formula_boundary(split.left_1)
+            and self._maybe_formula_boundary(split.right_2)
+        ):
+            return JOIN
+        return None
+    
+    def _maybe_formula_boundary(self, atom):
+        if atom is None:
+            return False
+        return (            
+            atom.type in (INT, LAT)
+            or atom.text in '()'
+        )
+    
+    def _maybe_formula_punctuation(self, atom):
+        if atom is None:
+            return False
+        return atom.text == ','
+    
+def create_segmenter():
+    segmenter = TokenSegmenter()
+    # segmenter.rules.append(rule) нельзя,
+    # поле rules ссылается на константу модуля, мы не хотим её менять
+    segmenter.rules = [*segmenter.rules, ChemicalRule()]
+    return segmenter
+    
+def tokenize_patched(txt):
+    segmenter = create_segmenter()
+    tokens = segmenter(txt)
+    return tokens
+
+tokens = tokenize_patched(EXAMPLE_TEXT)
+print(list(token.text for token in tokens))
+
+# %% [markdown]
+# Цель достигнута, химические формулы считаются одним токеном. Можно рассчитать количество слов.
+
+# %%
+import string
+
+
+def filter_words(tokens):
+    return (
+        token for token in tokens
+        if not all(c in string.punctuation for c in token.text)
+    )
+
+
+def count_words(txt):
+    return sum(
+        1 for _ in filter_words(tokenize_patched(txt))
+    )
+
+
+print(
+    list(
+        token.text for token in filter_words(
+            tokenize_patched(EXAMPLE_TEXT)
+        )
+    )
+)
+print(count_words(EXAMPLE_TEXT))
+
+# %% [markdown]
+# ### Овет к задаче 1
+
+# %%
+for topic, article in zip(topics, articles):
+    print(f'{topic:>24}, razdel: {count_words(article):>3}, regex: {count_words_by_regex(article):>3}')
+
+# %%
+# razdel находит не меньше слов, т.к. делит не только по пробелам.
+# пример расхождения
+initials = 'Иванов П.П.'
+print(
+    f'{initials} '
+    f'razdel: {count_words(initials)} '
+    f'regex: {count_words_by_regex(initials)}'
+)
+
+# %% [markdown]
+# ## Задача 2
+
+# %%
 
 # %% [markdown] id="krTGSvSORYSC"
 # _____
